1b05b8503de52733020002c410361fd224a1279a
jsjlim27 <jsjlim27@gmail.com>
2026-07-14 02:19:35 -0700

    barebones

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <SDL3/SDL.h>
 
-#include <stdbool.h>/* bool     */
-#include <stdlib.h> /* size_t   */
-#include <stdint.h> /* uint64_t */
+#include <stdio.h>   /* fprintf  */
+#include <stdbool.h> /* bool     */
+#include <stdlib.h>  /* size_t   */
+#include <stdint.h>  /* uint64_t */
 
 /*
 typedef enum {
@@ -22,16 +23,25 @@ typedef struct Drawable {
 } Drawable;
 */
 
+/* Record of every user-drawn mouse position. */
 typedef struct PointPool  {
-        SDL_FPoint *points;
+        SDL_FPoint *points; // lives in the heap so it can grow.
         int count;
         int cap;
 } PointPool;
 
+/*  */
 typedef struct Stroke {
-        int start;
+        int start; // index to the stroke's beginning position residing in
+                   // App.pool.points.
         int length;
 } Stroke;
+
+typedef struct StrokeList {
+        Stroke *data;
+        int count;
+        int cap;
+} StrokeList;
 
 typedef struct App {
         bool running;
@@ -44,12 +54,10 @@ typedef struct App {
 
         Stroke current_stroke;
 
-        Stroke *strokes;
-        int strokes_count;
-        int strokes_cap;
+        StrokeList stroke_list;
 
-        SDL_Color bg;
-        SDL_Color pen_color;
+        SDL_Color bg;        // canvas background color
+        SDL_Color pen_color; // draw tool color
 } App;
 
 bool App_init(App *app)
@@ -61,7 +69,7 @@ bool App_init(App *app)
         app->last_point.x = 0.0;
         app->last_point.y = 0.0;
 
-        // for now assume guaranteed success
+        /* Assume allocation always succeeds for now. */
         app->pool.points = malloc(65536 * sizeof(SDL_FPoint));
         app->pool.count = 0;
         app->pool.cap = 65536;
@@ -69,19 +77,20 @@ bool App_init(App *app)
         app->current_stroke.start = 0;
         app->current_stroke.length = 0;
 
-        // for now assume guaranteed success
-        app->strokes = malloc(16384 * sizeof(Stroke));
-        app->strokes_count = 0;
-        app->strokes_cap = 16384;
+        /* Assume allocation always succeeds for now. */
+        app->stroke_list.data = malloc(16384 * sizeof(Stroke));
+        app->stroke_list.count = 0;
+        app->stroke_list.cap = 16384;
 
         app->bg.r = 0;
         app->bg.g = 0;
         app->bg.b = 0;
         app->bg.a = 255;
 
-        app->pen_color.r = 255;
-        app->pen_color.g = 66;
-        app->pen_color.b = 200;
+        /* lavender */
+        app->pen_color.r = 211;
+        app->pen_color.g = 211;
+        app->pen_color.b = 255;
         app->pen_color.a = 255;
 
         return true;
@@ -105,6 +114,24 @@ bool point_pool_add(PointPool *pool, SDL_FPoint p)
         return true;
 }
 
+bool stroke_list_add(StrokeList *list, Stroke s)
+{
+        /* check if reached capacity */
+        if (list->count == list->cap) {
+                int new_cap = list->cap * 2;
+                Stroke *temp = realloc(list->data,
+                                       new_cap * sizeof(Stroke));
+
+                if (temp == NULL) { return false; }
+
+                list->data = temp;
+                list->cap = new_cap;
+        }
+
+        list->data[list->count++] = s;
+        return true;
+}
+
 /* only job should be handling inputs, no rendering */
 void handle_event(SDL_Event *event, App *app)
 {
@@ -123,25 +150,21 @@ void handle_event(SDL_Event *event, App *app)
                                 app->last_point = p;
                                 app->drawing = true;
                                 app->needs_redraw = true;
+                        } else if (event->button.button == SDL_BUTTON_X1) {
+                        } else if (event->button.button == SDL_BUTTON_X2) {
                         }
                         break;
                 }
 
                 case SDL_EVENT_MOUSE_BUTTON_UP: {
-                        if (app->strokes_count == app->strokes_cap) {
-                                int new_cap = 2 * app->strokes_cap;
-                                Stroke *temp = realloc(app->strokes, new_cap * sizeof(Stroke));
-                                //if (temp == NULL) { return; }
-                                // for now, just assume reallocation always succeed.
-                                app->strokes = temp;
-                                app->strokes_cap = new_cap;
+                        if (event->button.button == SDL_BUTTON_LEFT) {
+                                app->current_stroke.length = 
+                                        app->pool.count - app->current_stroke.start;
+
+                                stroke_list_add(&app->stroke_list, app->current_stroke);
+
+                                app->drawing = false;
                         }
-                        app->current_stroke.length = 
-                                app->pool.count - app->current_stroke.start;
-
-                        app->strokes[app->strokes_count++] = app->current_stroke;
-
-                        app->drawing = false;
                         break;
                 }
 
@@ -158,23 +181,51 @@ void handle_event(SDL_Event *event, App *app)
         }
 }
 
+/* simple render */
 void render(SDL_Renderer *renderer, App *app)
 {
         SDL_SetRenderDrawColor(renderer, 
-                               app->bg.r, app->bg.g, app->bg.b, app->bg.a);
+                               app->bg.r, 
+                               app->bg.g, 
+                               app->bg.b, 
+                               app->bg.a);
 
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer, 
-                               app->pen_color.r, app->pen_color.g, app->pen_color.b, app->pen_color.a);
+                               app->pen_color.r,
+                               app->pen_color.g,
+                               app->pen_color.b,
+                               app->pen_color.a);
 
-        for (int i = 0; i < app->strokes_count; i++) {
-                Stroke s = app->strokes[i];
-                for (int j = s.start; j < s.length; j++) {
+        /* draw all the finished strokes */
+        for (int i = 0; i < app->stroke_list.count; i++) {
+                Stroke stroke = app->stroke_list.data[i];
+                int end = stroke.start + stroke.length;
+                for (int j = stroke.start; j < end; j++) {
+                        SDL_FRect rect;
+                        rect.x = app->pool.points[j].x;
+                        rect.y = app->pool.points[j].y;
+                        rect.w = 10.0;
+                        rect.h = 10.0;
+                        // know that rect will be drawn off side. fix later.
+
+                        SDL_RenderFillRect(renderer, &rect);
                 }
         }
 
+        /* draw ongoing stroke */
+        for (int i = app->current_stroke.start; i < app->pool.count; i++) 
+        {
+                SDL_FRect rect;
+                rect.x = app->pool.points[i].x;
+                rect.y = app->pool.points[i].y;
+                rect.w = 10.0;
+                rect.h = 10.0;
+                // know that rect will be drawn off side. fix later.
 
+                SDL_RenderFillRect(renderer, &rect);
+        }
         SDL_RenderPresent(renderer);
 }
 
@@ -205,6 +256,11 @@ int main()
 
         SDL_Event event;
         while (app.running) {
+                while (SDL_PollEvent(&event)) {
+                        handle_event(&event, &app);
+                }
+                render(renderer, &app);
+                /*
                 Sint32 wait_ms = app.needs_redraw ? 0 : -1;
 
                 if (SDL_WaitEventTimeout(&event, wait_ms)) {
@@ -216,6 +272,7 @@ int main()
                         app.needs_redraw = false;
                         // last_render = now;
                 }
+                */
         }
 
         return 0;
